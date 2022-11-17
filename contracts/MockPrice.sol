7adeb301bf22bcb737fa2d583875376b9d0e0f31
WEIJING99 <weijing.zhang99@gmail.com>
2022-11-17 23:22:18 +1030

    add MockPrice Contract

---
+++ contracts/MockPrice.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+
+// This file create a MockAggregator (price feed smart contract) for testing.
